784d5d83a11fd54237898f115fc7ce1c9d1884b9
Arosha Udaraka <aroshaudaraka99@gmail.com>
2025-11-13 12:23:54 +0530

    spring boot backend initialized

---
+++ src/main/java/com/crimeLink/anayzer/AnayzerApplication.java
@@ -0,0 +1,13 @@
+package com.crimeLink.anayzer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnayzerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnayzerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/crimeLink/anayzer/AnayzerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crimeLink.anayzer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnayzerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
