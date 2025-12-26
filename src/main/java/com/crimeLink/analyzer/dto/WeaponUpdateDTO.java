683e8d1936f520b5f3c6d58d96278eabc33118e7
dasunithimasha <dasunithimasha25@gmail.com>
2025-12-26 18:23:32 +0530

    Create updateDTO class for weapon update

---
+++ src/main/java/com/crimeLink/analyzer/dto/WeaponUpdateDTO.java
@@ -0,0 +1,11 @@
+package com.crimeLink.analyzer.dto;
+
+import com.crimeLink.analyzer.enums.WeaponStatus;
+import lombok.Data;
+
+@Data
+public class WeaponUpdateDTO {
+    private String weaponType;
+    private WeaponStatus status;
+    private String remarks;
+}
