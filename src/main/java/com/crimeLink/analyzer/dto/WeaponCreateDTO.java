42dcafaef85372e7658173d940aeb51b920aa021
dasunithimasha <dasunithimasha25@gmail.com>
2025-12-26 18:23:54 +0530

    Create CreateDTO class for weapon save

---
+++ src/main/java/com/crimeLink/analyzer/dto/WeaponCreateDTO.java
@@ -0,0 +1,13 @@
+package com.crimeLink.analyzer.dto;
+
+import com.crimeLink.analyzer.enums.WeaponStatus;
+import lombok.Data;
+
+@Data
+public class WeaponCreateDTO {
+
+    private String serialNumber;
+    private String weaponType;
+    private WeaponStatus status;
+    private String remarks;
+}
