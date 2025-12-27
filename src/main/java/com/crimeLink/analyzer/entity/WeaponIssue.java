506d15ad523919dd33790f8c7ff7684bc4480065
dasunithimasha <dasunithimasha25@gmail.com>
2025-12-28 01:25:15 +0530

    Create WeaponIssue.java

--- src/main/java/com/crimeLink/analyzer/entity/WeaponIssue.java
+++ src/main/java/com/crimeLink/analyzer/entity/WeaponIssue.java
@@ -1,46 +1,54 @@
 package com.crimeLink.analyzer.entity;
 
+import com.crimeLink.analyzer.enums.WeaponIssueStatus;
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 import java.time.LocalDate;
 
 @Entity
+@Table(name = "weapon_issues")
 @Data
-@AllArgsConstructor
 @NoArgsConstructor
-@Table(name = "Weapon_Issue")
+@AllArgsConstructor
 public class WeaponIssue {
-
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer issueId;
 
+    /*  Weapon */
     @ManyToOne
     @JoinColumn(name = "weapon_serial", referencedColumnName = "serial_number")
     private Weapon weapon;
 
+    /*  ISSUED TO (uses the weapon) */
     @ManyToOne
-    @JoinColumn(name = "officer_id", referencedColumnName = "user_id")
-    private User officer;
+    @JoinColumn(name = "issued_to")
+    private User issuedTo;
 
-    /* Issue info */
-    private LocalDate issueDate;
-    private String issueTime;
+    /*  HANDED OVER BY (store / OIC) */
+    @ManyToOne
+    @JoinColumn(name = "handed_over_by")
+    private User handedOverBy;
 
+    /*  RECEIVED BY (return accepting officer) */
+    @ManyToOne
+    @JoinColumn(name = "received_by")
+    private User receivedBy;
+
+    /*  Issue info */
+    private LocalDate issuedDate;
+    private String issuedTime;
     private LocalDate dueDate;
 
-    /* Return info */
-    private LocalDate returnDate;
-    private String returnTime;
+    /*  Return info */
+    private LocalDate returnedDate;
+    private String returnedTime;
 
     private String issueNote;
-    private String returnRemark;
+    private String returnNote;
 
     @Enumerated(EnumType.STRING)
-    private WeaponIssue status;
-
+    private WeaponIssueStatus status;
 }
