bd4951824c9b687adab1f6ec5a0b20963858d00e
dasunithimasha <dasunithimasha25@gmail.com>
2025-12-27 18:46:11 +0530

    Add WeaponIssue Repository

---
+++ src/main/java/com/crimeLink/analyzer/repository/WeaponIssueRepository.java
@@ -0,0 +1,7 @@
+package com.crimeLink.analyzer.repository;
+
+import com.crimeLink.analyzer.entity.WeaponIssue;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface WeaponIssueRepository extends JpaRepository<WeaponIssue,Integer> {
+}
