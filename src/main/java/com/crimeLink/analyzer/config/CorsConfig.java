ea386e485a0327c4419083a3a2abc32158f9f09e
JinethBosilu <157296244+JinethBosilu@users.noreply.github.com>
2026-01-01 16:46:05 +0530

    CorsConfig: Updated

--- src/main/java/com/crimeLink/analyzer/config/CorsConfig.java
+++ src/main/java/com/crimeLink/analyzer/config/CorsConfig.java
@@ -15,7 +15,12 @@ public class CorsConfig {
     public CorsFilter corsFilter() {
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowCredentials(true);
-        config.setAllowedOrigins(Arrays.asList("http://localhost:5173", "http://localhost:3000"));
+        config.setAllowedOrigins(Arrays.asList(
+            "http://localhost:5173", 
+            "http://localhost:3000",
+            "http://127.0.0.1:3000",
+            "http://127.0.0.1:5173"
+        ));
         config.setAllowedHeaders(Arrays.asList("*"));
         config.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"));
         
