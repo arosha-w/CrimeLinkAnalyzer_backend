85f400c75128669f27d4de1e6f96a98d43f35073
dasunithimasha <dasunithimasha25@gmail.com>
2026-01-02 10:32:36 +0530

    Create Controllers to issue and return

--- src/main/java/com/crimeLink/analyzer/controller/WeaponIssueController.java
+++ src/main/java/com/crimeLink/analyzer/controller/WeaponIssueController.java
@@ -1,12 +1,12 @@
 package com.crimeLink.analyzer.controller;
 
+import com.crimeLink.analyzer.dto.IssueWeaponRequestDTO;
+import com.crimeLink.analyzer.dto.ReturnWeaponRequestDTO;
 import com.crimeLink.analyzer.dto.WeaponAddDTO;
 
 import com.crimeLink.analyzer.service.WeaponIssueService;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -24,5 +24,20 @@ public class WeaponIssueController {
     @GetMapping("/get-active-weapon")
     public List<WeaponAddDTO> getWeapons() {
         return weaponIssueService.getAvailableWeapons();
+    }
+
+    @PostMapping("/issue")
+    public ResponseEntity<?> issueWeapon(
+            @RequestBody IssueWeaponRequestDTO dto
+    ) {
+        weaponIssueService.issueWeapon(dto);
+        return ResponseEntity.ok("Weapon issued");
+    }
+
+
+    @PostMapping("/return")
+    public String returnWeapon(@RequestBody ReturnWeaponRequestDTO dto) {
+        weaponIssueService.returnWeapon(dto);
+        return "Weapon returned";
     }
 }
